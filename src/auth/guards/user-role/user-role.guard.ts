9d948f18c95e766d8e50ed592b22ed1894f43d92
SazukeR <reinaldojcg1@gmail.com>
2024-03-19 11:13:53 -0500

    Verify user role

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 
@@ -13,11 +13,29 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
-
     const validRoles: string[] = this.reflector.get('roles', context.getHandler());
 
-    console.log({ validRoles });
+    if (!validRoles) return true;
 
-    return true;
+    // console.log({ validRoles });
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user;
+
+    if ( !user ) {
+      throw new BadRequestException('User not found');
+    }
+
+    //console.log({ userRoles: user.roles });
+
+    for (const role of user.roles) {
+      if (validRoles.includes( role )) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${ user.fullName } need a valid role: [${ validRoles }]`
+    );
   }
 }
