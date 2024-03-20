8e18652eaa8bbb0fe2d7fd54c34d6c14d3cffa16
SazukeR <reinaldojcg1@gmail.com>
2024-03-20 18:21:08 -0500

    seed users

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
 import { initialData } from './data/seed-data';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 
 @Injectable()
@@ -9,16 +12,53 @@ export class SeedService {
 
   constructor(
     private readonly productsService: ProductsService,
+
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>
   ) {}
 
 
   async runSEED() {
 
-    await this.insertNewProducts();
+    await this.deleteTables();
+    const adminUser = await this.insertUsers();
+
+
+    await this.insertNewProducts( adminUser );
     return `SEED EXECUTED`;
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
+    await this.productsService.deleteAllProducts();
+
+    const queryBuilder = this.userRepository.createQueryBuilder();
+
+    await queryBuilder 
+      .delete()
+      .where({})
+      .execute()
+
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+
+
+
+    const users: User[] = [];
+
+
+    seedUsers.forEach( user => {
+      users.push( this.userRepository.create( user ) )
+    } );
+
+    const dbUsers = await this.userRepository.save( seedUsers );
+
+    return dbUsers[0];
+
+  }
+
+  private async insertNewProducts( user: User ) {
 
     await this.productsService.deleteAllProducts();
 
@@ -27,16 +67,11 @@ export class SeedService {
 
     const insertAllPromises = [];
 
-/*     products.forEach( product => {
-      insertAllPromises.push( this.productsService.create( product ) );
-    }) */
+    products.forEach( product => {
+      insertAllPromises.push( this.productsService.create( product, user ) );
+    }) 
 
     await Promise.all( insertAllPromises ); // espera a que todas las promesas se resuelvan para seguir con la ejecucion del codigo.
-
-
-
-
-
 
     return true;
   }
