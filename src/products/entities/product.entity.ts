afc1566ccc446b67b5342d27cc3eee3138613b75
SazukeR <reinaldojcg1@gmail.com>
2024-03-22 09:57:23 -0500

    ApiProperty

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,55 +1,83 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { ProductImage } from "./product-image.entity";
-import { User } from "src/auth/entities/user.entity";
-import { ApiProperty } from "@nestjs/swagger";
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
+import { ProductImage } from './';
+import { User } from '../../auth/entities/user.entity';
 
-
-@Entity({ name: "products" })
+@Entity({ name: 'products' })
 export class Product {
 
-    @ApiProperty()
-    @PrimaryGeneratedColumn("uuid")
+    @ApiProperty({
+        example: 'cd533345-f1f3-48c9-a62e-7dc2da50c8f8',
+        description: 'Product ID',
+        uniqueItems: true
+    })
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @ApiProperty()
-    @Column("text", {
+    @ApiProperty({
+        example: 'T-Shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true
+    })
+    @Column('text', {
         unique: true,
     })
     title: string;
 
-    @ApiProperty()
-    @Column('float', {
-        default: 0,
+    @ApiProperty({
+        example: 0,
+        description: 'Product price',
+    })
+    @Column('float',{
+        default: 0
     })
     price: number;
 
-    @ApiProperty()
-    @Column( {
-        type: "text",
-        nullable: true,
+    @ApiProperty({
+        example: 'Anim reprehenderit nulla in anim mollit minim irure commodo.',
+        description: 'Product description',
+        default: null,
+    })
+    @Column({
+        type: 'text',
+        nullable: true
     })
     description: string;
 
-    @ApiProperty()
-    @Column("text", {
-        unique: true,
+    @ApiProperty({
+        example: 't_shirt_teslo',
+        description: 'Product SLUG - for SEO',
+        uniqueItems: true
+    })
+    @Column('text', {
+        unique: true
     })
     slug: string;
 
-    @ApiProperty()
-    @Column("int", {
-        default: 0,
+    @ApiProperty({
+        example: 10,
+        description: 'Product stock',
+        default: 0
+    })
+    @Column('int', {
+        default: 0
     })
     stock: number;
 
-    @ApiProperty()
-    @Column('text', {
-        array: true,
+    @ApiProperty({
+        example: ['M','XL','XXL'],
+        description: 'Product sizes',
+    })
+    @Column('text',{
+        array: true
     })
     sizes: string[];
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'women',
+        description: 'Product gender',
+    })
     @Column('text')
     gender: string;
 
@@ -60,47 +88,45 @@ export class Product {
     })
     tags: string[];
 
-
     // images
     @ApiProperty()
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
-        { cascade: true, eager: true } // con el eager en true hara que el oneby tambien traiga las imagenes relacionadas a los productos
+        { cascade: true, eager: true }
     )
     images?: ProductImage[];
+
 
     @ManyToOne(
         () => User,
         ( user ) => user.product,
-        { eager: true },
+        { eager: true }
     )
     user: User
+
 
     @BeforeInsert()
     checkSlugInsert() {
 
         if ( !this.slug ) {
-            this.slug = this.title
+            this.slug = this.title;
         }
 
         this.slug = this.slug
             .toLowerCase()
-            .replaceAll(' ', '_')
-            .replaceAll("'", '')
-            
+            .replaceAll(' ','_')
+            .replaceAll("'",'')
+
     }
-
-
 
     @BeforeUpdate()
-    checkSlugUpdated() {
-
+    checkSlugUpdate() {
         this.slug = this.slug
             .toLowerCase()
-            .replaceAll(' ', '_')
-            .replaceAll("'", '')
-
+            .replaceAll(' ','_')
+            .replaceAll("'",'')
     }
+
 
 }
