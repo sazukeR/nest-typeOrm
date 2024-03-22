5223ef5df003af1d7f71b6073154e0a6011b2380
SazukeR <reinaldojcg1@gmail.com>
2024-03-22 16:44:46 -0500

    implements gateways

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,18 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    console.log(`Client conected, ${client.id}`)
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconected, ${client.id}`)
+  }
+
 }
