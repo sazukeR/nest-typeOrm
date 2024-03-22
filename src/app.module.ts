d65740100d0158729bb82b1aaca21c907e9bfb05
SazukeR <reinaldojcg1@gmail.com>
2024-03-22 11:58:43 -0500

    Websocket first settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
+import { MessagesWsModule } from './messages-ws/messages-ws.module';
 
 
 @Module({
@@ -31,7 +32,9 @@ import { AuthModule } from './auth/auth.module';
 
     FilesModule,
 
-    AuthModule
+    AuthModule,
+
+    MessagesWsModule
   ],
 })
 export class AppModule {}
