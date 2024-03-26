04adf42f35d5f630a55dda75ee5861b8e7ef3da9
SazukeR <reinaldojcg1@gmail.com>
2024-03-26 17:22:31 -0500

    Link socket to user

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,18 +2,40 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+      private readonly messagesWsService: MessagesWsService,
 
-  handleConnection(client: Socket) {
-    //console.log(`Client conected, ${client.id}`)
+      private readonly jwtService: JwtService,
+    ) {}
 
-    this.messagesWsService.registerClient( client );
+  async handleConnection(client: Socket) {
+    //console.log(`Client conected, ${client.id}`);
+
+    const token = client.handshake.headers.authentication as string;
+
+    let payload: JwtPayload;
+
+    try {
+
+      payload = this.jwtService.verify( token );
+      await this.messagesWsService.registerClient( client, payload.id );
+
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+   // console.log({ payload })
+   // console.log( token )
+
+
 
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
 
@@ -51,7 +73,7 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
     // Emitir a todos
     this.wss.emit('message-from-server', {
-      fullName: 'Soy Yo!!',
+      fullName: this.messagesWsService.getUserFulName(client.id),
       message: payload.message || 'no-message'
     })
 
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 interface ConnectedClients {
-    [id: string]: Socket
+    [id: string]: {
+        socket: Socket,
+        user: User,
+    }
 }
 
 @Injectable()
 export class MessagesWsService {
 
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>
+    ) {}
+
     private connectedClients: ConnectedClients = {}
 
-    registerClient( client: Socket ) {
-        this.connectedClients[client.id] = client;
+    async registerClient( client: Socket, userId: string ) {
+
+        const user = await this.userRepository.findOneBy({ id: userId });
+        if (!user) throw new Error('User not found');
+        if (!user.isActive) throw new Error('User not active');
+
+        this.connectedClients[client.id] = {
+            socket: client,
+            user: user,
+        };
     }
 
     removeClient( clientId: string ) {
@@ -19,7 +38,12 @@ export class MessagesWsService {
     }
 
     getConnectedClients(): string[] {
+       // console.log(this.connectedClients)
         return Object.keys( this.connectedClients );
+    }
+
+    getUserFulName( socketId: string ) {
+        return this.connectedClients[socketId].user.fullName;
     }
 
 }
